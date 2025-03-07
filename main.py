de1ebeb072dd99f7f5234f053a5fbe53075912df
CiaL-eXstaSe <j.l.vietinghoff@gmail.com>
2025-03-07 10:47:33 +0100

    added game basic game loop and gitignore

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+# Because this is a smaller project, and we don't risk conflicting import names, 
+# we're going to use a wildcard import for convenience. 
+# In a larger project, you'd want to import only the constants you need
+from constants import *
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    # Create the screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    running = True
+    while running:
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        pygame.display.flip()
+    
+    # Quit Pygame properly
+    pygame.quit()
+
 if __name__ == "__main__":
     main()
