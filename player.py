8835af7e0484c1ee5aa1a13ef202cc1801f8a73f
CiaL-eXstaSe <j.l.vietinghoff@gmail.com>
2025-03-14 10:32:47 +0100

    revised shooting delay implementation

--- player.py
+++ player.py
@@ -1,16 +1,18 @@
 import pygame
+from constants import *
 from circleshape import CircleShape
-from constants import (PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, 
-                      PLAYER_SHOT_SPEED, PLAYER_SHOOT_COOLDOWN)
 from shot import Shot
+
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation = 0  # Rotation angle in degrees
-        self.shoot_timer = 0  # Timer for shooting cooldown
+        self.rotation = 0
+        self.shoot_timer = 0
 
-    # in the player class
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -18,46 +20,32 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-            
-    def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
-        
-    def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
-
-    def move(self, dt):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
 
     def update(self, dt):
-        # Update shoot timer
-        if self.shoot_timer > 0:
-            self.shoot_timer -= dt
-
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
-            self.rotate(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
 
     def shoot(self):
-        # Only shoot if the cooldown timer is 0 or less
-        if self.shoot_timer <= 0:
-            # Create a shot at the player's position
-            shot = Shot(self.position.x, self.position.y)
-            
-            # Set the shot's velocity
-            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
-            
-            # Reset the shoot timer
-            self.shoot_timer = PLAYER_SHOOT_COOLDOWN
-            
-            
-    
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
